f8f8e0a67a41bdfa7d68344316f5fdef0ac48322
nomaddevs1 <farouqsiwoku@gmail.com>
2024-02-15 13:27:51 -0600

    Setting up vercel

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,16 @@
-const express = require('express');
-const dotenv = require('dotenv');
+import express, { Request, Response } from 'express'
+import dotenv from 'dotenv'
 
 
 dotenv.config()
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
     console.log('YAY!')
     res.send("Welcome to the world of Arduino with Node.js installed")
 })
 
-app.listen(port,'0.0.0.0', (err, res) => { 
-    if (err) { 
-        console.error(err)
-        return;
-    }
+app.listen(port, () => { 
     console.log(`[server]: Server is running at http://localhost:${port}`)
 })
