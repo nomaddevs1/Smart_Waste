cf27aaed8c4a6437ec5cd11de8c6e151f24cef00
nickingenito <ningenito@gmail.com>
2024-03-23 13:33:37 -0500

    Add page listing all organizations and their boards

--- gps-client/src/pages/Dashboard.tsx
+++ gps-client/src/pages/Dashboard.tsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import {
   Flex,
   IconButton,
-  useDisclosure
+  useDisclosure,
+  Button,
 } from "@chakra-ui/react";
 import { WifiMedium } from "@phosphor-icons/react";
 import WifiSetupModal from "../component/WifiSetupModal";
@@ -10,12 +11,14 @@ import FirestoreService from "../db/db";
 import { useAuth } from "../context/UserAuthContext";
 import { DocumentData } from "firebase/firestore";
 import Cards from "../component/Card";
+import AllBoards from "../component/AllBoards";
 
 const Dashboard = () => {
   const [selectedBoard] = useState<string | null>(null);
   const [boards, setBoard] = useState<[] | DocumentData[]>([]);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const {user} = useAuth()!
+  const [viewAllBoards, setViewAllBoards] = useState(false);
 
 useEffect(() => {
   if (user) {
@@ -43,13 +46,18 @@ useEffect(() => {
     onOpen();
   };
 
+  const toggleAllBoards = () => {
+    setViewAllBoards(!viewAllBoards);
+  }
+
   return (
     <Flex
       width="100wh"
       marginTop={"100px"}
       overflowY={"auto"}
     >
-      <Cards boards={boards}/>
+      {viewAllBoards ? <AllBoards /> : <Cards boards={boards}/>}
+
       <WifiSetupModal
         isOpen={isOpen}
         onClose={onClose}
@@ -57,7 +65,7 @@ useEffect(() => {
       />
 
       <IconButton
-        icon={<WifiMedium size={32} color="#366fba" />}
+        icon={<WifiMedium size={32} color="white" />}
         colorScheme="teal"
         borderRadius="full"
         position="absolute"
@@ -66,6 +74,9 @@ useEffect(() => {
         aria-label="Add Button"
         onClick={() => openModal()}
       />
+      <Button pos="absolute" top="90px" right="5" variant="link" onClick={toggleAllBoards}>
+        {viewAllBoards ? "Return to Dashboard" : "Show All Boards"}
+      </Button>
     </Flex>
   );
 };
