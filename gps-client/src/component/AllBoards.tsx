b8f6e6244535bea325c46d928a0d53133394da9b
nickingenito <ningenito@gmail.com>
2024-04-03 10:22:51 -0500

    Update all board card view

--- gps-client/src/component/AllBoards.tsx
+++ gps-client/src/component/AllBoards.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Box, Text, List, ListItem } from "@chakra-ui/react";
+import { Box, Heading, Stack, StackDivider, Card, CardHeader, Button } from "@chakra-ui/react";
 import FirestoreService from "../db/db";
 
 const AllBoards = () => {
@@ -20,20 +20,20 @@ const AllBoards = () => {
 
   return (
     <Box ml="2rem">
-      <Text fontSize="2xl" mb="4">All Organizations</Text>
+      <Heading fontSize="2xl" mb="4">All Organizations</Heading>
       {orgs.map((org: any) => (
-        <Box ml="2rem" mb="4" key={org.id}>
-          <Text fontSize="lg">{org.data.orgName}</Text>
-          <List ml="1rem">
+        <Card mb="4" key={org.id} width="30rem" borderRadius="10px">
+          <CardHeader display="flex" alignItems="center" justifyContent="space-between" bgColor="blue.900" borderTopRadius="5px">
+            <Heading fontSize="lg" color="white">{org.data.orgName}</Heading>
+            <Button colorScheme="teal" color="white" justifySelf="end">Buy Board</Button>
+          </CardHeader>
+          <Stack ml="1rem" divider={<StackDivider />}>
             {org.data.boards.map((board: any) => (
-              <ListItem key={board}>Serial Number: {board}</ListItem>
+              <Box p="1rem" key={board}>Serial Number: {board}</Box>
             ))}
-          </List>
-        </Box>
+          </Stack>
+        </Card>
       ))}
-      <List>
-
-      </List>
     </Box>
   );
 }
