c989d04c8652170795c3475dd0f9e0943b087688
nomaddevs1 <farouqsiwoku@gmail.com>
2024-04-04 13:37:57 -0500

    Fix issue with status

--- server/src/services/boardServices.ts
+++ server/src/services/boardServices.ts
@@ -1,17 +1,24 @@
-import { db } from '../firebase/adminConfig'; // Your initialized Firebase Admin
+import { db } from "../firebase/adminConfig"; // Your initialized Firebase Admin
 
-export const updateBoardLocation = async (serialNumber: string, lat: string, lng: string) => {
-  try{
-  const boardDocRef = db.collection('boards').doc(serialNumber);
-  if(parseInt(lat)!== 0 && parseInt(lng) !== 0){
-  await boardDocRef.update({
-    lat: lat,
-    lng: lng,
-    status: "full"
-  });
-}
-}catch (e) {
-  console.log(e);
-}
-
+export const updateBoardLocation = async (
+  serialNumber: string,
+  lat: string,
+  lng: string
+) => {
+  try {
+    const boardDocRef = db.collection("boards").doc(serialNumber);
+    if (parseInt(lat) !== 0 && parseInt(lng) !== 0) {
+      await boardDocRef.update({
+        lat: lat,
+        lng: lng,
+        status: "full",
+      });
+    } else {
+      await boardDocRef.update({
+        status: "full",
+      });
+    }
+  } catch (e) {
+    console.log(e);
+  }
 };
