48fbf8fa390520adbba957f6c9562ea20347c071
nickingenito <ningenito@gmail.com>
2024-04-21 16:48:18 -0500

    Update logo and add circuit design diagram

--- gps-client/src/component/Logo.tsx
+++ gps-client/src/component/Logo.tsx
@@ -1,12 +1,12 @@
 import { Box, Text } from "@chakra-ui/react";
-import {ReactComponent as LogoIcon} from '../assets/bin_full.svg';
+import {ReactComponent as LogoIcon} from '../assets/logo.svg';
 
 export const  Logo = (props: any) => {
   return (
     <Box {...props} display="flex" alignItems="center">
-      <LogoIcon width="40px" height="40px"/>
+      <LogoIcon width="45px" height="45px"/>
       <Text ml="10px" fontSize="2xl" fontWeight="bold">
-        Logo Name
+        Smart Waste
       </Text>
     </Box>
   );
