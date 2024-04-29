10fdb25f43b9b4bdaa1577574646c9a91fc29f28
nomaddevs1 <farouqsiwoku@gmail.com>
2024-04-29 12:41:48 -0500

    add navigation to the header logo

--- gps-client/src/component/Logo.tsx
+++ gps-client/src/component/Logo.tsx
@@ -1,10 +1,12 @@
 import { Box, Text } from "@chakra-ui/react";
 import {ReactComponent as LogoIcon} from '../assets/logo.svg';
+import { useNavigate } from "react-router-dom";
 
 export const  Logo = (props: any) => {
+  const navigate = useNavigate()
   return (
     <Box {...props} display="flex" alignItems="center">
-      <LogoIcon width="45px" height="45px"/>
+      <LogoIcon width="45px" height="45px" onClick={() => {navigate("/dashboard")}}/>
       <Text ml="10px" fontSize="2xl" fontWeight="bold">
         Smart Waste
       </Text>
